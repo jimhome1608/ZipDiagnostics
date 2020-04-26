42b1f9da42e0e132687d0b8c964a978864335d34
Jim Clark <jimhome1608@gmail.com>
2020-04-26 18:59:50 +1000

    db modified to allow multiple tests on same sample

--- TestStationManagement/test_station_db_schema.sql
+++ TestStationManagement/test_station_db_schema.sql
@@ -32,13 +32,14 @@ CREATE TABLE `error_log` (
   `key_word` varchar(255) DEFAULT NULL,
   `data` longtext,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `samples` */
 
 CREATE TABLE `samples` (
   `station_id` varchar(255) NOT NULL,
   `id` int(11) NOT NULL,
+  `test_id` int(11) NOT NULL DEFAULT '0',
   `sample_time` datetime NOT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `family_name` varchar(255) DEFAULT NULL,
@@ -61,7 +62,7 @@ CREATE TABLE `samples` (
   `test_start_user_id` int(11) DEFAULT NULL,
   `test_result_user_id` int(11) DEFAULT NULL,
   `result_file` longtext,
-  PRIMARY KEY (`station_id`,`id`)
+  PRIMARY KEY (`station_id`,`id`,`test_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Table structure for table `samples_history` */
@@ -70,6 +71,7 @@ CREATE TABLE `samples_history` (
   `history_id` int(11) NOT NULL,
   `station_id` varchar(255) NOT NULL,
   `id` int(11) NOT NULL,
+  `test_id` int(11) NOT NULL DEFAULT '0',
   `sample_time` datetime NOT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `family_name` varchar(255) DEFAULT NULL,
@@ -92,7 +94,7 @@ CREATE TABLE `samples_history` (
   `test_start_user_id` int(11) DEFAULT NULL,
   `test_result_user_id` int(11) DEFAULT NULL,
   `result_file` longtext,
-  PRIMARY KEY (`history_id`,`station_id`,`id`)
+  PRIMARY KEY (`history_id`,`station_id`,`id`,`test_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Table structure for table `settings` */
@@ -175,26 +177,26 @@ DELIMITER ;
 
 DELIMITER $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `save_sample`(IN _station_id varchar(255),  _id int ,  _sample_time datetime, _first_name varchar(255),  _family_name varchar(255), _date_of_birth datetime,  
+CREATE DEFINER=`root`@`localhost` PROCEDURE `save_sample`(IN _station_id varchar(255),  _id int ,  _test_id int , _sample_time datetime, _first_name varchar(255),  _family_name varchar(255), _date_of_birth datetime,  
           _postcode varchar(255),  _phone varchar(255),  _email varchar(255), _notes longtext,  _sample_id varchar(255),  _test_result varchar(255),  _test_status varchar(255),  
 	  _test_start_time datetime,  _result_import_time datetime,  _test_time datetime, _web_saved int,  _sample_user_name varchar(255),  _test_start_user_name varchar(255),  
           _test_result_user_name varchar(255),  _sample_user_id int, _test_start_user_id int,  _test_result_user_id int , _result_file longtext)
 BEGIN
    declare new_id int;
-   if exists (select * from samples where station_id = _station_id and id = _id) then
-	insert into samples_history (select get_next_history_id(),station_id,  id,  sample_time, first_name,  family_name, date_of_birth,  postcode,  phone,  email, notes,  sample_id,  
+   if exists (select * from samples where station_id = _station_id and id = _id and test_id = _test_id) then
+	insert into samples_history (select get_next_history_id(),station_id,  id, test_id,  sample_time, first_name,  family_name, date_of_birth,  postcode,  phone,  email, notes,  sample_id,  
          test_result,  test_status,  test_start_time,  result_import_time,  test_time, web_saved,  sample_user_name,  test_start_user_name,  
          test_result_user_name,  sample_user_id, test_start_user_id,  test_result_user_id,  result_file
-        from samples where station_id = _station_id and id = _id );
-        delete from samples where station_id = _station_id and id = _id;
+        from samples where station_id = _station_id and id = _id and test_id = _test_id );
+        delete from samples where station_id = _station_id and id = _id  and test_id = _test_id;
    end if;
    insert into `samples` 
-	(station_id,  id,  sample_time, first_name,  family_name, date_of_birth,  postcode,  phone,  email, notes,  sample_id,  
+	(station_id,  id, test_id,  sample_time, first_name,  family_name, date_of_birth,  postcode,  phone,  email, notes,  sample_id,  
          test_result,  test_status,  test_start_time,  result_import_time,  test_time, web_saved,  sample_user_name,  test_start_user_name,  
          test_result_user_name,  sample_user_id, test_start_user_id,  test_result_user_id,  result_file
 	)
 	values
-	(_station_id,  _id,  _sample_time, _first_name,  _family_name, _date_of_birth,  _postcode,  _phone,  _email, _notes,  _sample_id,  
+	(_station_id,  _id,  _test_id, _sample_time, _first_name,  _family_name, _date_of_birth,  _postcode,  _phone,  _email, _notes,  _sample_id,  
          _test_result,  _test_status,  _test_start_time,  _result_import_time,  _test_time, _web_saved,  _sample_user_name,  _test_start_user_name,  
          _test_result_user_name,  _sample_user_id, _test_start_user_id,  _test_result_user_id,  _result_file
 	);
@@ -273,21 +275,6 @@ DELIMITER $$
 CREATE DEFINER=`root`@`localhost` FUNCTION `get_sample_id`(_station_id varchar(255), _id int) RETURNS varchar(255) CHARSET latin1
 BEGIN   
    return Concat(Upper(_station_id),'S',_id);		
-END$$
-
-DELIMITER ;
-
-/* Function  structure for function  `get_test_barcode` */
-
-DELIMITER $$
-
-CREATE DEFINER=`root`@`localhost` FUNCTION `get_test_barcode`(_station_id varchar(255),_sample_id int) RETURNS varchar(4096) CHARSET latin1
-BEGIN
-   declare sample_save_time varchar(255);
-   declare sid varchar(255);
-   set sample_save_time = (select DATE_FORMAT(sample_time,'%d-%b-%Y') from samples where station_id = _station_id and id = _sample_id); 
-   set sid = (select sample_id from samples where station_id = _station_id and id = _sample_id); 
-   return Concat('',sid,':',Upper(sample_save_time));		
 END$$
 
 DELIMITER ;
